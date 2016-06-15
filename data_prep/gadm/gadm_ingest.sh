b1a372f8a5a81f39cf31b7fa1cd3a1ee6fe5edba
userz <sgoodman@aiddata.org>
2016-06-15 15:34:14 -0400

    Test ingest.

--- data_prep/gadm/gadm_ingest.sh
+++ data_prep/gadm/gadm_ingest.sh
@@ -5,7 +5,7 @@ src="${HOME}"/active/"$branch"
 
 version_dir='gadm'${version}
 
-data_dir='/sciclone/aiddata10/REU/data/boundaries/'${version_dir}
+data_dir='/sciclone/aiddata10/REU/data/boundaries_test/'${version_dir}
 
 if [ ! -d $data_dir ]; then
     echo 'Could not find download directory for GADM version' ${version}
@@ -13,16 +13,18 @@ if [ ! -d $data_dir ]; then
 fi
 
 
-for i in $data_dir/*/*.shp; do
+for i in $data_dir/*/*.geojson; do
 
-    name=$(basename ${i} .shp)
+    name=$(basename ${i} .geojson)
 
     bnd_dir=$data_dir/$name
 
     abs_path=$(readlink -f ${bnd_dir})
 
+    print $i
+    print $abs_path
     # add to asdf
-    python "${src}"/asdf/src/add_gadm.py ${branch} ${abs_path} ${version} auto
+    echo 'pxython "${src}"/asdf/src/add_gadm.py ${branch} ${abs_path} ${version} auto'
 
 done
 
