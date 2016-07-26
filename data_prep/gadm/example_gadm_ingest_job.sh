950f850e356749c842ad3b2766d614265f67b225
userz <sgoodman@aiddata.org>
2016-07-26 14:08:06 -0400

    Update gadm job.

--- data_prep/gadm/example_gadm_ingest_job.sh
+++ data_prep/gadm/example_gadm_ingest_job.sh
@@ -1,9 +1,10 @@
 #!/bin/tcsh
 #PBS -N ax-gadm
-#PBS -l nodes=3:c18c:ppn=4
-#PBS -l walltime=24:00:00
+#PBS -l nodes=7:c18c:ppn=16
+#PBS -l walltime=72:00:00
 #PBS -j oe
 
-set f = ${HOME}/active/develop/asdf-datasets/data_prep/gadm/gadm_ingest.py
+set branch = develop
+set f = ${HOME}/active/${branch}/asdf-datasets/data_prep/gadm/gadm_ingest.py
 
-mpirun --mca mpi_warn_on_fork 0 --map-by node -np 12 python-mpi $f develop 2.8 parallel update true
+mpirun --mca mpi_warn_on_fork 0 --map-by node -np 112 python-mpi $f $branch 2.8 parallel update false
