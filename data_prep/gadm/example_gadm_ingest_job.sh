20c2345b9fd8ded2e2d494688bc026a4c906ffdc
userz <sgoodman@aiddata.org>
2016-07-27 12:33:11 -0400

    Reduce workers for gadm ingest job.

--- data_prep/gadm/example_gadm_ingest_job.sh
+++ data_prep/gadm/example_gadm_ingest_job.sh
@@ -1,10 +1,10 @@
 #!/bin/tcsh
 #PBS -N ax-gadm
-#PBS -l nodes=7:c18c:ppn=16
+#PBS -l nodes=4:c18c:ppn=16
 #PBS -l walltime=24:00:00
 #PBS -j oe
 
 set branch = develop
 set f = ${HOME}/active/${branch}/asdf-datasets/data_prep/gadm/gadm_ingest.py
 
-mpirun --mca mpi_warn_on_fork 0 --map-by node -np 112 python-mpi $f $branch 2.8 parallel update 0
+mpirun --mca mpi_warn_on_fork 0 --map-by node -np 64 python-mpi $f $branch 2.8 parallel update 0
