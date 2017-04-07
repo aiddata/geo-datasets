335df74cb0429159b6e614dd6a0e0a44c1f66c31
userw <sgoodman@aiddata.org>
2017-04-07 14:13:53 -0400

    Add new wdpa iucn cat and air pollution scripts

---
+++ data_prep/air_pollution/build_air_pollution_rasters.py
@@ -0,0 +1,45 @@
+"""
+rasterize ambient air pollution data to 0.1 degree resolution
+"""
+
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+from distancerasters import rasterize, export_raster
+
+
+# -----------------------------------------------------------------------------
+
+
+input_csv = "/sciclone/aiddata10/REU/raw/ambient_air_pollution/GBD2013final.csv"
+
+pixel_size = 0.1
+
+col_prefixes = ['o3', 'fus_calibrated']
+
+years = [1990, 1995, 2000, 2005, 2010, 2011, 2012, 2013]
+
+output_base = "/sciclone/aiddata10/REU/data/rasters/external/global/ambient_air_pollution"
+
+
+# -----------------------------------------------------------------------------
+
+
+df = pd.read_csv(input_csv, delimiter=",", encoding='utf-8')
+
+df['geometry'] = df.apply(lambda z: Point(z['x'], z['y']), axis=1)
+
+gdf = gpd.GeoDataFrame(df)
+
+for pre in col_prefixes:
+    for y in years:
+        rasterize(
+            gdf,
+            attribute="{0}_{1}".format(pre, y),
+            pixel_size=pixel_size,
+            bounds=gdf.geometry.total_bounds,
+            output="{0}/{1}/{1}_{2}.tif".format(output_base, pre, y))
+
+
+
---
+++ data_prep/wdpa/build_iucn_cat_raster.py
@@ -0,0 +1,92 @@
+"""
+build iucn categorical raster from wdpa data
+"""
+
+import fiona
+import rasterio
+import numpy as np
+from affine import Affine
+from rasterio import features
+
+from distancerasters import rasterize, export_raster
+
+
+# -----------------------------------------------------------------------------
+
+
+shp_path = "/sciclone/aiddata10/REU/raw/wdpa/WDPA_Apr2017_Public/WDPA_Apr2017_Public.gdb"
+
+pixel_size = 0.01
+
+field_name = "IUCN_CAT"
+field_values = [
+    "Ia", "Ib", "II", "III", "IV", "V", "VI",
+    "Not Applicable", "Not Assigned", "Not Reported"
+]
+
+final_output = "/sciclone/aiddata10/REU/data/rasters/external/global/wdpa/iucn_cat_201704/wdpa_iucn_cat_201704.tif"
+
+
+# -----------------------------------------------------------------------------
+
+
+# load data and init key variables
+
+layers = fiona.listlayers(shp_path)
+poly_layer = [i for i in layers if "poly" in i]
+
+if len(poly_layer) > 1:
+    raise Exception("multiple potential polygon layers found")
+elif len(poly_layer) == 0:
+    raise Exception("no potential polygon layer found")
+
+features_input = fiona.open(shp_path, layer=poly_layer[0])
+
+
+try:
+    pixel_size = float(pixel_size)
+except:
+    raise Exception("Invalid pixel size (could not be converted to float)")
+
+(minx, miny, maxx, maxy) = features_input.bounds
+
+out_shape = (int(round((maxy - miny) / pixel_size))+1,
+             int(round((maxx - minx) / pixel_size))+1)
+
+affine = Affine(pixel_size, 0, minx,
+                0, -pixel_size, maxy)
+
+
+# build intermediary rasters for individual categories
+
+cat_layers = list()
+
+for cat in field_values:
+
+    features_filtered = [f for f in features_input
+                         if f["properties"][field_name] == cat]
+
+    print "selected {0} features for field: {1}".format(
+        len(features_filtered), cat)
+
+    if len(features_filtered) == 0:
+        print "no feature selected for year {0}".format(cat)
+        pass
+
+    cat_raster = rasterize(features_filtered, affine=affine, shape=out_shape)
+
+    cat_layers.append(cat_raster)
+
+
+# merge category rasters and output
+
+output_raster = np.zeros(shape=(out_shape[0], out_shape[1]))
+
+for index in range(len(cat_layers)):
+    output_raster = output_raster + cat_layers[index] * (index + 1)
+    output_raster = np.where(output_raster > (index + 1), 11, output_raster)
+
+
+export_raster(output_raster, affine=affine, path=final_output)
+
+
