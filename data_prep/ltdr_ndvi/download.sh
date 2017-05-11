3a2630175202e30616665519c8b2ea60cd162c5f
sgoodm <sgoodman@aiddata.org>
2017-05-11 11:03:41 -0400

    Add ltdr download script and viirs unzip script

---
+++ data_prep/ltdr_ndvi/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# download all ltdr data (ver4)
+wget -r -c -N -P /sciclone/aiddata10/REU/geo/raw --retr-symlinks=yes ftp://ltdr.nascom.nasa.gov/allData/Ver4/
---
+++ data_prep/viirs/extract.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+src="/sciclone/aiddata10/REU/geo/raw/viirs/source/dnb_composites/v10"
+
+dst="/sciclone/aiddata10/REU/geo/raw/viirs/monthly_vcmcfg_dnb_composites_v10"
+
+cd $src
+
+
+find -maxdepth 1 -type d -regex "./[2][0-9][0-9][0-9][0-1][0-9]$" -print0 |
+while read -d $'\0' folder
+do
+
+    month=$(basename "$folder")
+    echo $month
+
+    mkdir -p $dst/$month
+
+    find $src/$month/vcmcfg -maxdepth 1 -name \*tgz -exec tar -zxv -C $dst/$month -f {} \;
+
+done
+
