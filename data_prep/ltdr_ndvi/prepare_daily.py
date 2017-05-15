4ea5522a5a6a7642321e84df1f675304a8dc151b
sgoodm <sgoodman@aiddata.org>
2017-05-15 16:23:04 -0400

    Update ltdr ndvi prep (tested locally) and remove old files

--- data_prep/ltdr_ndvi/prepare_daily.py
+++ data_prep/ltdr_ndvi/prepare_daily.py
@@ -1,10 +1,8 @@
 """
-for use with NDVI product from LTDR raw dataset (AVH13C1)
+for use with NDVI product from LTDR raw dataset
 
-example LTDR product file names
+example LTDR product file names (ndvi product code is AVH13C1)
 
-AVH02C1.A1981299.N07.004.2013228133954.hdf
-AVH09C1.A1981211.N07.004.2013228083053.hdf
 AVH13C1.A1981181.N07.004.2013227210959.hdf
 
 split file name by "."
@@ -19,16 +17,13 @@ full file name - "AVH13C1.A1981181.N07.004.2013227210959.hdf"
 4     processed date      2013227210959
 5     extension           hdf
 
-ndvi product code is AVH13C1
 """
 
 import os
+import errno
+from collections import OrderedDict
 import numpy as np
 from osgeo import gdal, osr
-
-
-input_path = "/home/userw/Desktop/AVH13C1.A2010153.N19.004.2015206181729.hdf"
-output_dir = "/home/userw/Desktop"
 
 
 def process_data(input_path, output_dir):
@@ -49,9 +44,12 @@ def process_data(input_path, output_dir):
 
     """
     output_path = "{0}/{1}.tif".format(
-        output_dir
-        os.path.basename(os.path.splitext(input_path)[0]))
+        output_dir,
+        os.path.basename(os.path.splitext(input_path)[0])
+    )
 
+    print input_path
+    print output_path
 
     # open the dataset and subdataset
     hdf_ds = gdal.Open(input_path, gdal.GA_ReadOnly)
@@ -124,45 +122,61 @@ def process_data(input_path, output_dir):
 
 # -----------------------------------------------------------------------------
 
-path_base = "/sciclone/aiddata10/REU/geo/raw/ltdr/ltdr.nascom.nasa.gov/allData/Ver4"
 
 
-# reference object used to eliminate duplicate year / day combos
-# when overlaps between sensors exists, always use data from newer sensor
-ref = {}
+src_base = "/sciclone/aiddata10/REU/geo/raw/ltdr/ltdr.nascom.nasa.gov/allData/Ver4"
 
-# list of all [year, day] combos
+dst_base = "/sciclone/aiddata10/REU/data/ltdr/ndvi/daily"
+
+
+# build list of all [year, day] combos
+
+###
+
+# use accept/deny to manually limit which
+# sensors or years will run
+# all values must be strings
 
 # sensor_accept = []
+# sensor_deny= []
+
+year_accept = ['1986']
+# year_deny= []
+
+###
 
 # get sensors
 sensors = [
-    name for name in os.listdir(path_base)
-    if os.path.isdir(os.path.join(path_base, name))
+    name for name in os.listdir(src_base)
+    if os.path.isdir(os.path.join(src_base, name))
         and name.startswith("N")
         and len(name) == 3
         # and name in sensor_accept
+        # and name not in sensor_deny
 ]
 sensors.sort()
 
 
-# use limited sensors for testing
-# sensors = ['2001']
+# reference object used to eliminate duplicate year / day combos
+# when overlaps between sensors exists, always use data from newer sensor
+ref = OrderedDict()
 
 for sensor in sensors:
 
     # get years for sensors
-    path_sensor = path_base +"/"+ sensor
+    path_sensor = src_base +"/"+ sensor
     years = [
         name for name in os.listdir(path_sensor)
         if os.path.isdir(os.path.join(path_sensor, name))
+            and name in year_accept
+            # and name not in year_deny
     ]
     years.sort()
 
     for year in years:
 
         if not year in ref:
-            ref[year] = {}
+            ref[year] = OrderedDict()
 
         # get days for year
         path_year = path_sensor +"/"+ year
@@ -179,22 +193,71 @@ for sensor in sensors:
             filename = filename[:-4]
             day = filename.split(".")[1][5:]
 
+            # check if a file for same day as current file exists
+            # and if a file does exist, check if current sensor is newer
             if not day in ref[year] or int(sensor[1:]) > int(ref[year][day][0][1:]):
-                # print "\n" + str(year) +" "+ str(day) +" "+ str(sensor) +" "+ str(filename)
                 ref[year][day] = [sensor, filename]
+                # print "\n {0} {1} {2} {3}".format(year, day, sensor, filename)
 
 
-# list final [sensor, year, day] combos from reference object
-# qlist = [ref[year][day] + [year, day] for day in ref[year] for year in ref if year in ref and day in ref[year]]
 
+# qlist item format = [sensor, filename, year, day]
 qlist = []
 for year in ref:
     for day in ref[year]:
         qlist.append(ref[year][day] + [year, day])
-        # print qlist
 
-for c in range(len(qlist)):
 
+
+# -----------------------------------------------------------------------------
+
+
+def prep_data(task, input_base, output_base):
+
+    sensor, filename, year, day = item
+
+    src_file = os.path.join(src_base, sensor, year, filename + ".hdf")
+
+    dst_dir = os.path.join(dst_base, year)
+
+    try:
+        os.makedirs(dst_dir)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+    print "{0} {1} {2}".format(sensor, year, day)
+    process_data(src_file, dst_dir)
+
+
+# -----------------------------------------------------------------------------
+
+
+mode = "parallel"
+# mode = "serial"
+
+if mode == "parallel":
+
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    c = rank
+    while c < len(qlist):
+
+        prep_data(qlist[c], src_base, dst_base)
+        c += size
+
+elif mode == "serial":
+
+    for c in range(len(qlist)):
+        prep_data(qlist[c], src_base, dst_base)
+
+
+else:
+    raise Exception("Invalid `mode` value for script.")
 
 
 
