f5ea8347ab4c2814703f15f1b332276b6a9c8a86
userw <sgoodman@aiddata.org>
2017-06-02 16:09:32 -0400

    Build dataframe with scene info [landsat7]

--- data_prep/landsat7/prepare_data.py
+++ data_prep/landsat7/prepare_data.py
@@ -3,10 +3,9 @@ import os
 import pandas as pd
 
 file_df = pd.read_csv("test_scene_list.txt", header=None)
-file_list = list(x[0])
+file_list = list(file_df[0])
 
 data = []
-path_row_combos = []
 for f in file_list:
     scene = os.path.basename(f)
     path = scene[4:7]
@@ -14,10 +13,17 @@ for f in file_list:
     year = scene[10:14]
     month = scene[14:16]
     day = scene[16:18]
-    info = (path, row, year, month, day)
-    data.append((info, f))
-    print info
-    path_row_combos.append((path,row))
+    info = (path, row, year, month, day, f)
+    data.append(info)
+
+
+columns = ("path", "row", "year", "month", "day", "file")
+
+data_df = pd.DataFrame(data, columns=columns)
+
+
+
+
 
 
 
