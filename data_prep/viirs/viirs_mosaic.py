a3be6cfaad2f98f58d7f6587169052dfab76a9ed
userw <sgoodman@aiddata.org>
2017-06-18 17:23:56 -0400

    Parallelize mosaic script [viirs]

--- data_prep/viirs/viirs_mosaic.py
+++ data_prep/viirs/viirs_mosaic.py
@@ -1,14 +1,12 @@
 
 import os
 import glob
+import rasterio
 from rasterio.merge import merge as scene_mosaic
 
 
-tile_data = "/sciclone/aiddata10/REU/geo/raw/viirs/vcmcfg_dnb_composites_v10/filtered_monthly"
 
-mosaic_data = "/sciclone/aiddata10/REU/geo/data/rasters/external/global/viirs/vcmcfg_dnb_composites_v10/monthly"
-
-for i in os.listdir(tile_data):
+def run_mosaic(i):
 
     print "Running {0}".format(i)
 
@@ -39,4 +37,49 @@ for i in os.listdir(tile_data):
     mosaic = rasterio.open(mosaic_output_path, 'w', **mosaic_profile)
     mosaic.write(mosaic_array)
     mosaic.close()
+
+
+
+
+tile_data = "/sciclone/aiddata10/REU/geo/raw/viirs/vcmcfg_dnb_composites_v10/filtered_monthly"
+
+mosaic_data = "/sciclone/aiddata10/REU/geo/data/rasters/external/global/viirs/vcmcfg_dnb_composites_v10/monthly"
+
+
+mode = "parallel"
+# mode = "serial"
+
+
+qlist = os.listdir(tile_data)
+
+
+if mode == "parallel":
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    c = rank
+    while c < len(qlist):
+
+        try:
+            run_mosaic(qlist[c])
+        except Exception as e:
+            print "Error processing tile section: {0}".format(c)
+            raise
+
+        c += size
+
+    comm.Barrier()
+
+elif mode == "serial":
+
+    for c in range(len(qlist)):
+        run_mosaic(qlist[c])
+
+else:
+    raise Exception("Invalid `mode` value for script.")
+
+
+
 
