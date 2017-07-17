afb69e2a4ad8cb0f8ef88b9722042480df64c24d
userz <sgoodman@aiddata.org>
2017-07-17 12:45:51 -0400

    Add comments [esa lc]

--- data_prep/esa_landcover/build_layers.py
+++ data_prep/esa_landcover/build_layers.py
@@ -1,13 +1,12 @@
 """
+Source data is single tif containing 24 bands, where each band is a year.
+These bands needs to be split into separate tif files
 
 Download from and unzip multiband tif to input path specificed below
-
 http://maps.elie.ucl.ac.be/CCI/viewer/download.php
-
 
 qsub -I -l nodes=1:c18c:ppn=16 -l walltime=24:00:00
 python ~/active/master/asdf-datasets/data_prep/esa_landcover/build_layers.py
-
 """
 
 import rasterio
