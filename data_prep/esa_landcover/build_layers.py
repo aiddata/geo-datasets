1635982226b3b514a73118a2410fe70d88685b28
userz <sgoodman@aiddata.org>
2017-07-17 12:42:22 -0400

    Add data path [esa lc]

--- data_prep/esa_landcover/build_layers.py
+++ data_prep/esa_landcover/build_layers.py
@@ -12,9 +12,22 @@ python ~/active/master/asdf-datasets/data_prep/esa_landcover/build_layers.py
 
 import rasterio
 
+import os
+import errno
+
+
+def make_dir(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
 
 src_path = "/sciclone/aiddata10/REU/geo/raw/esa_landcover_v207/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif"
 dst_dir_path = "/sciclone/aiddata10/REU/geo/data/rasters/esa_landcover_v207"
+
+make_dir(dst_dir_path)
 
 
 start_year = 1992
@@ -35,6 +48,7 @@ with rasterio.open(src_path) as src:
         array = src.read(band)
 
         dst_path = "{0}/esa_lc_{1}.tif".format(dst_dir_path, year)
+        make_dir(os.path.dirname(dst_path))
 
         with rasterio.open(dst_path, 'w', **profile) as dst:
             dst.write(array, 1)
