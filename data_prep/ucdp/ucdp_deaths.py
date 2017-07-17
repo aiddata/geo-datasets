9d73c98a60815bc01ac1051a4aa800a31a58dd9e
Miranda Lv <zlv@aiddata.org>
2017-07-17 16:05:54 -0400

    correct ucdp death script

--- data_prep/ucdp/ucdp_deaths.py
+++ data_prep/ucdp/ucdp_deaths.py
@@ -4,39 +4,53 @@ import geopandas as gpd
 from shapely.geometry import Point
 from distancerasters import rasterize
 
-# input_path = r"/Users/miranda/Documents/AidData/sciclone/datasets/UCDP_GED_conflict/ucdp-ged-poly-v-1-1-shape/UCDPGEDpolyyear.shp"
-
-input_path = "/sciclone/home10/zlv/datasets/ucdp/ged171.csv"
+input_path = r"/Users/miranda/Documents/AidData/sciclone/datasets/UCDP_GED_conflict/ged171.csv"
+# input_path = "/sciclone/home10/zlv/datasets/ucdp/ged171.csv"
 
 df = pd.read_csv(input_path, sep=',', encoding="utf-8")
 df['geometry'] = df.apply(lambda z: Point(z['longitude'], z['latitude']), axis=1)
 gdf = gpd.GeoDataFrame(df)
 
 
-pixel_size = 0.1
-data_field = 'best' # best estimate of deaths
+pixel_size = 0.01
 
-field_values = {1: "State-based", 2: "Non-state", 3: "One-sided"}
+output_dir = "/Users/miranda/Documents/AidData/sciclone/datasets/UCDP_GED_conflict/output"
+# output_dir = "/sciclone/data20/zlv/ucdp/fatality"
 
-# output_dir = "/Users/miranda/Documents/AidData/sciclone/datasets/UCDP_GED_conflict/output"
 
-output_dir = "/sciclone/data20/zlv/ucdp/fatality"
+# create a new id to group points that are in near location
 
-for field in field_values.keys():
+def round_point(row):
 
-    field_gdf = gdf[gdf["type_of_violence"] == field]
+    newlat = int(row["latitude"]/pixel_size) * pixel_size
 
-    for year in range(1989, 2016):
+    newlon = int(row["longitude"]/pixel_size) * pixel_size
 
-        rst_gdf = field_gdf[field_gdf["year"] == year]
+    newid = str(newlat) + "_" + str(newlon)
 
-        print "rasterize year", year
+    return newid
 
-        rasterize(
-            rst_gdf,
-            attribute=data_field,
-            pixel_size=pixel_size,
-            bounds=rst_gdf.geometry.total_bounds,
-            output="{0}/{1}_{2}_{3}.tif".format(output_dir, "ucdp_fatality", field_values[field], year),
-            fill=-1,
-            nodata=-1)
+
+gdf["newid"] = gdf.apply(lambda row: round_point(row), axis=1)
+
+
+for year in range(1989, 2017):
+
+    filtered = gdf[gdf["year"] == year]
+
+    groups = filtered.groupby(["newid"])["best"].sum().reset_index()
+
+    groups = groups.rename(columns={"best":"sum_best"})
+
+    rst_gdf = filtered.merge(groups,how="left", on="newid")
+
+    print "rasterize year", year
+
+    rasterize(
+        rst_gdf,
+        attribute="sum_best",
+        pixel_size=pixel_size,
+        bounds=rst_gdf.geometry.total_bounds,
+        output="{0}/{1}_{2}.tif".format(output_dir, "ucdp_fatality", year),
+        fill=-1,
+        nodata=-1)
