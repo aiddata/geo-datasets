671056cac59509171a958446a6dfc27ccfbf6019
userz <sgoodman@aiddata.org>
2017-07-21 12:29:32 -0400

    Update ucdp death/conflict scripts (added original to 'type_and_year' subdir [ucdp]

--- data_prep/ucdp/ucdp_deaths.py
+++ data_prep/ucdp/ucdp_deaths.py
@@ -1,56 +1,84 @@
+"""
+sum of deaths from conflict
 
+UCDP Georeferenced Event Dataset (GED) Global
+http://ucdp.uu.se/downloads/
+
+
+Download to raw directory (include version in path)
+
+Update raw directory source and output in data directory
+(with corresponding version in path)
+"""
+
+import os
+import errno
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
 from distancerasters import rasterize
 
-input_path = r"/Users/miranda/Documents/AidData/sciclone/datasets/UCDP_GED_conflict/ged171.csv"
-# input_path = "/sciclone/home10/zlv/datasets/ucdp/ged171.csv"
 
-df = pd.read_csv(input_path, sep=',', encoding="utf-8")
-df['geometry'] = df.apply(lambda z: Point(z['longitude'], z['latitude']), axis=1)
-gdf = gpd.GeoDataFrame(df)
+# -----------------------------------------------------------------------------
 
+
+input_path = "/sciclone/aiddata10/REU/geo/raw/ucdp/ged_global_v171/ged171.csv"
+
+output_dir = "/sciclone/aiddata10/REU/geo/data/rasters/ucdp/ged_global_171"
 
 pixel_size = 0.01
 
-output_dir = "/Users/miranda/Documents/AidData/sciclone/datasets/UCDP_GED_conflict/output"
-# output_dir = "/sciclone/data20/zlv/ucdp/fatality"
+
+# -----------------------------------------------------------------------------
 
 
-# create a new id to group points that are in near location
+def make_dir(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
 
 def round_point(row):
-
-    newlat = int(row["latitude"]/pixel_size) * pixel_size
-
-    newlon = int(row["longitude"]/pixel_size) * pixel_size
-
-    newid = str(newlat) + "_" + str(newlon)
-
+    """ create a new id to group points that are in near location
+    """
+    newlon = round(row["longitude"]/pixel_size) * pixel_size
+    newlat = round(row["latitude"]/pixel_size) * pixel_size
+    newid = str(newlon) + "_" + str(newlat)
     return newid
 
 
-gdf["newid"] = gdf.apply(lambda row: round_point(row), axis=1)
+make_dir(output_dir)
+
+df = pd.read_csv(input_path, sep=',', encoding="utf-8")
+df["newid"] = df.apply(lambda row: round_point(row), axis=1)
+
+
+df['geometry'] = df.apply(
+        lambda z: Point(z['longitude'], z['latitude']), axis=1)
+gdf = gpd.GeoDataFrame(df)
 
 
 for year in range(1989, 2017):
 
-    filtered = gdf[gdf["year"] == year]
+    print "Rasterizing year: {0}".format(year)
 
-    groups = filtered.groupby(["newid"])["best"].sum().reset_index()
+    filtered = df[df["year"] == year]
 
-    groups = groups.rename(columns={"best":"sum_best"})
+    grouped = filtered.groupby(["newid"]).agg(
+        {'longitude': 'last', 'latitude': 'last', 'best': 'sum'}).reset_index()
 
-    rst_gdf = filtered.merge(groups,how="left", on="newid")
+    grouped['geometry'] = grouped.apply(
+        lambda z: Point(z['longitude'], z['latitude']), axis=1)
 
-    print "rasterize year", year
+    tmp_gdf = gpd.GeoDataFrame(grouped)
 
-    rasterize(
-        rst_gdf,
-        attribute="sum_best",
+    array, affine = rasterize(
+        tmp_gdf,
+        attribute="best",
         pixel_size=pixel_size,
-        bounds=rst_gdf.geometry.total_bounds,
-        output="{0}/{1}_{2}.tif".format(output_dir, "ucdp_fatality", year),
-        fill=-1,
+        bounds=gdf.geometry.total_bounds,
+        output="{0}/ucdp_deaths_{1}.tif".format(output_dir, year),
+        fill=0,
         nodata=-1)
