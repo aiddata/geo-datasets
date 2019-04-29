61da52f42acf873d56a6f7be41a020630a1155c0
userz <sgoodman@aiddata.wm.edu>
2019-04-29 15:21:06 -0400

    Update ltdr download

--- ltdr_ndvi/download.sh
+++ ltdr_ndvi/download.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-# download all ltdr data (ver4)
-wget -r -c -N -P /sciclone/aiddata10/REU/geo/raw/ltdr --retr-symlinks=yes ftp://ltdr.nascom.nasa.gov/allData/Ver4/
+app_key="PUT YOUR APP KEY HERE"
+
+base_url="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/465"
+
+dst="/sciclone/aiddata10/REU/geo/raw/ltdr/LAADS"
+
+sensors=(
+    N07_AVH13C1
+    N09_AVH13C1
+    N11_AVH13C1
+    N14_AVH13C1
+    N16_AVH13C1
+    N18_AVH13C1
+    N19_AVH13C1
+)
+
+for s in "${sensors[@]}"; do
+	src=${base_url}/$s
+    echo $src
+    wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=2 ${src} --header "Authorization: Bearer ${app_key}" -P ${dst}
+done
--- ltdr_ndvi/readme.md
+++ ltdr_ndvi/readme.md
@@ -1,3 +1,6 @@
+
+Add app key to download script
+
 Run download script
 
 Submit job to run processing script
