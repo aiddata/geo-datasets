b3918568fb1af2bc8550d49d32d9db97b5f096bd
sgoodm <sgoodman@aiddata.wm.edu>
2021-04-21 10:39:07 -0400

    Cleanup cru ts code

--- cru_ts/extract_data.py
+++ cru_ts/extract_data.py
@@ -1,23 +1,35 @@
 """
 Python 3.8
 
+Be sure to create (if needed) and initialize the Conda environment before running this script
+`conda create -n geodata-cru python=3.8 rasterio netcdf4`
+`conda activate geodata-cru`
+
 Make sure to review all variables (static file names, years) when using this script for a new version of cru data
 
 
+The netcdf to geotiff conversion part of this script is fairly quick running in serial (<10minutes)
+but would be easy to adapt for basic parellelization.
 
-This script is fairly quick running in serial (<15minutes) but would be easy to adapt
-for basic parellelization.
+    Suggested parallelization steps would be to just add the in_path variable for each
+    cru var to an instance of the band_temporal_list and combine all of the resulting lists.
+    The trios in this list could then each be passed to the extract_layer function using any
+    parellization map function.
 
-Suggested parallelization steps would be to just add the in_path variable for each
-cru var to an instance of the band_temporal_list and combine all of the resulting lists.
-The trios in this list could then each be passed to the extract_layer function using any
-parellization map function.
+
+The yearly aggregation is parellized using the multiprocessing package and only requires access
+to multiple cores. This can be run on SciClone's Hima nodes using the following command to start a job:
+`qsub -I -l nodes=1:hima:nogpu:ppn=64 -l walltime=24:00:00`
 
 """
 
 import os
 import rasterio
 import numpy as np
+import multiprocessing
+
+
+base_path = "/sciclone/aiddata10/REU/geo"
 
 # this should match the dir names used for raw data and to be used for processed data
 cru_label = "cru_ts_4.05"
@@ -76,9 +88,9 @@ def extract_layer(input, out_path, band):
 
 for var in var_list:
     print("Running variable:", var)
-    var_dir = os.path.join("/sciclone/aiddata10/REU/geo/data/rasters", cru_label, "monthly", var)
+    var_dir = os.path.join(base_path, "data/rasters", cru_label, "monthly", var)
     os.makedirs(var_dir, exist_ok=True)
-    in_path = "netcdf:/sciclone/aiddata10/REU/geo/raw/{0}/cru_ts4.05.1901.2020.{1}.dat.nc:{1}".format(cru_label, var)
+    in_path = "netcdf:{0}/raw/{1}/cru_ts4.05.1901.2020.{2}.dat.nc:{2}".format(base_path, cru_label, var)
     src = rasterio.open(in_path)
     for band, temporal in band_temporal_list:
         print("\tprocessing", temporal)
@@ -144,15 +156,15 @@ def aggregate_rasters(file_list, method="mean"):
 
 
 
-import multiprocessing
 
 print(multiprocessing.cpu_count())
 
+cores_to_use = multiprocessing.cpu_count()
 
 def run_yearly_data(year, method, var, cru_label):
     print("Running:", var, method, year)
-    src_base = os.path.join("/sciclone/aiddata10/REU/geo/data/rasters", cru_label, "monthly", var)
-    dst_base = os.path.join("/sciclone/aiddata10/REU/geo/data/rasters", cru_label, "yearly", var, method)
+    src_base = os.path.join(base_path, "data/rasters", cru_label, "monthly", var)
+    dst_base = os.path.join(base_path, "data/rasters", cru_label, "yearly", var, method)
     year_files = sorted([os.path.join(src_base, i) for i in os.listdir(src_base) if "cru.{}.{}".format(var, year) in i])
     year_mask = "cru.{}.YYYY.tif".format(var)
     year_path = os.path.join(dst_base, year_mask.replace("YYYY", str(year)))
@@ -169,12 +181,12 @@ method_list = ["mean", "min", "max", "sum"]
 qlist = []
 for var in var_list:
     for method in method_list:
-        dst_base = os.path.join("/sciclone/aiddata10/REU/geo/data/rasters", cru_label, "yearly", var, method)
+        dst_base = os.path.join(base_path, "/data/rasters", cru_label, "yearly", var, method)
         os.makedirs(dst_base, exist_ok=True)
         for year in years:
             qlist.append([year, method, var, cru_label])
 
 
 
-with multiprocessing.Pool(processes=64) as pool:
+with multiprocessing.Pool(processes=cores_to_use) as pool:
     results = pool.starmap(run_yearly_data, qlist)
