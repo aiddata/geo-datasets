5be99b2481d25b75224b2dd863644924b26ef3b4
Jacob Hall <email@jacobhall.net>
2022-10-21 15:39:36 -0400

    update conda environment
    - conda environment build on vortex cluster
    - add boxsdk, dask-jobqueue, prefect-dask

--- pm25/create_env.sh
+++ pm25/create_env.sh
@@ -1,4 +1,4 @@
 conda create -n pm25 python=3.9
 conda activate pm25
 conda install affine netcdf4 numpy pandas proj
-pip install mpi4py prefect rasterio
+pip install mpi4py prefect rasterio boxsdk prefect-dask dask-jobqueue
