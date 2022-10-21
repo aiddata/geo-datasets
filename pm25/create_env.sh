eea7a8fca8d1a31565f779d1c1e03632c56ee495
Jacob Hall <email@jacobhall.net>
2022-10-21 16:21:04 -0400

    install boxsdk JWT dependencies in create_env.sh

--- pm25/create_env.sh
+++ pm25/create_env.sh
@@ -1,4 +1,4 @@
 conda create -n pm25 python=3.9
 conda activate pm25
 conda install affine netcdf4 numpy pandas proj
-pip install mpi4py prefect rasterio boxsdk prefect-dask dask-jobqueue
+pip install mpi4py prefect rasterio "boxsdk[jwt]" prefect-dask dask-jobqueue
