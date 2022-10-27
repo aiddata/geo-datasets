6e329ccc87465673d59b71a506963c4a95be773d
Jacob Hall <email@jacobhall.net>
2022-10-27 16:45:36 -0400

    update create_env.sh for pm2.5

--- pm25/create_env.sh
+++ pm25/create_env.sh
@@ -1,4 +1,4 @@
-conda create -n pm25 python=3.9
+conda create -n pm25 python=3.8 affine bokeh dask-jobqueue netcdf4 numpy proj rasterio
 conda activate pm25
-conda install affine netcdf4 numpy pandas proj
-pip install mpi4py prefect rasterio "boxsdk[jwt]" prefect-dask dask-jobqueue
+conda install -c conda-forge prefect prefect-dask
+pip install "boxsdk[jwt]" mpi4py pandas
