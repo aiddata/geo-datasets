95106236848e0995284010826372c4f29f248858
sgoodm <sgoodman@aiddata.wm.edu>
2022-12-01 10:45:23 -0500

    Update flow class to include all args

--- malaria_atlas_project/flow.py
+++ malaria_atlas_project/flow.py
@@ -5,8 +5,8 @@ from main import MalariaAtlasProject
 
 
 @flow
-def malaria_atlas_project(raw_dir, output_dir, years, dataset, overwrite, backend, run_parallel, max_workers):
+def malaria_atlas_project(raw_dir, output_dir, years, dataset, overwrite, backend, task_runner, run_parallel, max_workers):
 
     class_instance = MalariaAtlasProject(raw_dir, output_dir, years, dataset, overwrite)
 
-    class_instance.run(backend=backend, task_runner="dask", run_parallel=run_parallel, max_workers=max_workers)
+    class_instance.run(backend=backend, task_runner=task_runner, run_parallel=run_parallel, max_workers=max_workers)
