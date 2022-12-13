2667a7b4ff31c0fdfb93d1856943fa79ee33df68
Seth Goodman <smgoodman@vx10.sciclone.wm.edu>
2022-12-13 12:24:41 -0500

    Debug

--- malaria_atlas_project/flow.py
+++ malaria_atlas_project/flow.py
@@ -674,6 +674,6 @@ def malaria_atlas_project(raw_dir, output_dir, years, dataset, overwrite_downloa
     #from malaria import MalariaAtlasProject
 
 
-    class_instance = malaria.MalariaAtlasProject(raw_dir, output_dir, years, dataset, overwrite_download, overwrite_processing)
+    class_instance = MalariaAtlasProject(raw_dir, output_dir, years, dataset, overwrite_download, overwrite_processing)
 
     class_instance.run(backend=backend, task_runner=task_runner, run_parallel=run_parallel, max_workers=max_workers, log_dir=log_dir)
