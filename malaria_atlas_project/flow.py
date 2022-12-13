2a5e85b79269ad51ea3c27aa58d07ef885178ca5
Seth Goodman <sgoodman@aiddata.wm.edu>
2022-12-13 15:26:11 -0500

    Cleanup malaria files for merge

--- malaria_atlas_project/flow.py
+++ malaria_atlas_project/flow.py
@@ -13,24 +13,13 @@ config.read(config_file)
 block_name = config["deploy"]["storage_block"]
 GitHub.load(block_name).get_directory('global_scripts')
 
-# sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'global_scripts'))
 sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), config["github"]["directory"]))
 
-# import malaria
 from main import MalariaAtlasProject
 
 
 @flow
 def malaria_atlas_project(raw_dir, output_dir, years, dataset, overwrite_download, overwrite_processing, backend, task_runner, run_parallel, max_workers, log_dir):
-
-    # import sys, os
-    # sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), config["github"]["directory"]))
-    # print(os.listdir(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), config["github"]["directory"])))
-    # print(sys.path)
-
-    # import malaria
-    #from malaria import MalariaAtlasProject
-
 
     class_instance = MalariaAtlasProject(raw_dir, output_dir, years, dataset, overwrite_download, overwrite_processing)
 
