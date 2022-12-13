76d6c8aafdf38f00b459b8a15f2d09fa255b51ae
Seth Goodman <smgoodman@vx10.sciclone.wm.edu>
2022-12-13 12:01:31 -0500

    Debug

--- malaria_atlas_project/flow.py
+++ malaria_atlas_project/flow.py
@@ -24,7 +24,7 @@ from malaria import MalariaAtlasProject
 def malaria_atlas_project(raw_dir, output_dir, years, dataset, overwrite_download, overwrite_processing, backend, task_runner, run_parallel, max_workers, log_dir):
     sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), config["github"]["directory"]))
     import os
-    print(os.listdir(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), config["github"]["directory"]))
+    print(os.listdir(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), config["github"]["directory"])))
     print(sys.path)
 
     import malaria
