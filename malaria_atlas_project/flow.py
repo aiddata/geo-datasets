0ef2d8b4545e95d1824d36739a12e134d304ef1a
Seth Goodman <smgoodman@vx10.sciclone.wm.edu>
2022-12-13 10:34:01 -0500

    Correct import in flow

--- malaria_atlas_project/flow.py
+++ malaria_atlas_project/flow.py
@@ -17,7 +17,7 @@ GitHub.load(block_name).get_directory('global_scripts')
 sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), config["github"]["directory"]))
 
 
-from main import MalariaAtlasProject
+from malaria import MalariaAtlasProject
 
 
 @flow
