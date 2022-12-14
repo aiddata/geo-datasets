12aa0622e4141ac7f14f53131af502b1b221cb8c
Seth Goodman <sgoodman@aiddata.wm.edu>
2022-12-14 13:12:04 -0500

    Debug

--- debug/main_combo.py
+++ debug/main_combo.py
@@ -14,7 +14,7 @@ vortex_cluster_kwargs = {
         "source /usr/local/anaconda3-2021.05/etc/profile.d/conda.csh", 
         "module load gcc/9.3.0 openmpi/3.1.4/gcc-9.3.0 anaconda3/2021.05",
         "conda activate geodata38",
-        "set tmpdir=`mktemp -d",
+        "set tmpdir=`mktemp -d`",
         "cd $tmpdir",
         "git clone -b malaria_debug https://github.com/aiddata/geo-datasets.git",
         "cd geo-datasets/debug",
