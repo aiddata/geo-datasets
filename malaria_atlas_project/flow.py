04397da3bb716c3561b8923558ba374b0a682acf
Seth Goodman <sgoodman@aiddata.wm.edu>
2022-12-15 12:31:56 -0500

    Create timestamped dir for logs within log dir for all logs (including dask-jobqueue worker outputs)

--- malaria_atlas_project/flow.py
+++ malaria_atlas_project/flow.py
@@ -1,5 +1,8 @@
-import sys, os
+import sys
+import os
+from pathlib import Path
 from configparser import ConfigParser
+from datetime import datetime
 
 from prefect import flow
 from prefect.filesystems import GitHub
@@ -20,6 +23,12 @@ from main import MalariaAtlasProject
 
 @flow
 def malaria_atlas_project(raw_dir, output_dir, years, dataset, overwrite_download, overwrite_processing, backend, task_runner, run_parallel, max_workers, cores_per_process, log_dir):
+ 
+    timestamp = datetime.today()
+    time_format_str: str="%Y_%m_%d_%H_%M"
+    time_str = timestamp.strftime(time_format_str)
+    timestamp_log_dir = Path(log_dir / time_str)
+    timestamp_log_dir.mkdir(parents=True, exists_ok=True)
 
     cluster_kwargs = {
         "shebang": "#!/bin/tcsh",
@@ -28,6 +37,11 @@ def malaria_atlas_project(raw_dir, output_dir, years, dataset, overwrite_downloa
         "processes": 12,
         "memory": "30GB",
         "interface": "ib0",
+        "job_extra_directives": [
+            "#PBS -j oe",
+            # "#PBS -o ",
+            # "#PBS -e ",
+        ],
         "job_script_prologue": [
             "source /usr/local/anaconda3-2021.05/etc/profile.d/conda.csh",
             "module load gcc/9.3.0 openmpi/3.1.4/gcc-9.3.0 anaconda3/2021.05",
@@ -38,9 +52,9 @@ def malaria_atlas_project(raw_dir, output_dir, years, dataset, overwrite_downloa
             "cd geo-datasets/malaria_atlas_project",
             "cp ../global_scripts/* ."
         ],
-        "log_directory": log_dir
+        "log_directory": timestamp_log_dir
     }
 
     class_instance = MalariaAtlasProject(raw_dir, output_dir, years, dataset, overwrite_download, overwrite_processing)
 
-    class_instance.run(backend=backend, task_runner=task_runner, run_parallel=run_parallel, max_workers=max_workers, cores_per_process=cores_per_process, log_dir=log_dir, cluster_kwargs=cluster_kwargs)
+    class_instance.run(backend=backend, task_runner=task_runner, run_parallel=run_parallel, max_workers=max_workers, cores_per_process=cores_per_process, log_dir=timestamp_log_dir, cluster_kwargs=cluster_kwargs)
--- malaria_atlas_project/main.py
+++ malaria_atlas_project/main.py
@@ -26,6 +26,7 @@ from copy import copy
 from pathlib import Path
 from zipfile import ZipFile
 from configparser import ConfigParser
+from datetime import datetime
 
 import rasterio
 from rasterio import windows
@@ -241,6 +242,14 @@ if __name__ == "__main__":
 
     config_dict = get_config_dict()
 
+    log_dir = config_dict["log_dir"]
+    timestamp = datetime.today()
+    time_format_str: str="%Y_%m_%d_%H_%M"
+    time_str = timestamp.strftime(time_format_str)
+    timestamp_log_dir = Path(log_dir / time_str)
+    timestamp_log_dir.mkdir(parents=True, exists_ok=True)
+
+
     class_instance = MalariaAtlasProject(config_dict["raw_dir"], config_dict["output_dir"], config_dict["years"], config_dict["dataset"], config_dict["overwrite_download"], config_dict["overwrite_processing"])
 
-    class_instance.run(backend=config_dict["backend"], task_runner=config_dict["task_runner"], run_parallel=config_dict["run_parallel"], max_workers=config_dict["max_workers"], cores_per_process=config_dict["cores_per_process"], log_dir=config_dict["log_dir"])
+    class_instance.run(backend=config_dict["backend"], task_runner=config_dict["task_runner"], run_parallel=config_dict["run_parallel"], max_workers=config_dict["max_workers"], cores_per_process=config_dict["cores_per_process"], log_dir=timestamp_log_dir)
