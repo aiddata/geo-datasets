401153e5c3079ebdae116b57d8028f0eb879c361
Seth Goodman <sgoodman@aiddata.wm.edu>
2023-01-03 17:17:42 -0500

    Adjust flow job memory use to avoid running over max avail

--- esa_landcover/flow.py
+++ esa_landcover/flow.py
@@ -32,8 +32,8 @@ def esa_landcover(raw_dir, output_dir, years, overwrite_download, overwrite_proc
     cluster_kwargs = {
         "shebang": "#!/bin/tcsh",
         "resource_spec": "nodes=1:c18a:ppn=12",
-        "cores": 1,
-        "processes": 1,
+        "cores": 12,
+        "processes": 2,
         "memory": "30GB",
         "interface": "ib0",
         "job_extra_directives": [
