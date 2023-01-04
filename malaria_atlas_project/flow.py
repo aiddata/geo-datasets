9c284b31d5c85fc51b57bc1bde7dbfcc99987c70
Seth Goodman <sgoodman@aiddata.wm.edu>
2023-01-04 10:35:17 -0500

    Debug

--- malaria_atlas_project/flow.py
+++ malaria_atlas_project/flow.py
@@ -42,9 +42,9 @@ def malaria_atlas_project(raw_dir, output_dir, years, dataset, overwrite_downloa
             # "#PBS -e ",
         ],
         "job_script_prologue": [
-            # "source /usr/local/anaconda3-2021.05/etc/profile.d/conda.csh",
-            # "module load anaconda3/2021.05",
-            # "conda activate geodata38",
+            "source /usr/local/anaconda3-2021.05/etc/profile.d/conda.csh",
+            "module loadanaconda3/2021.05",
+            "conda activate geodata38",
             f"cd {tmp_dir}",
         ],
         "log_directory": str(timestamp_log_dir)
