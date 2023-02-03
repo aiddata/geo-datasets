c62942dd1f785e51b40a5a8fb5331ec4f5766424
Cheyenne Hwang <92409641+cmhwang@users.noreply.github.com>
2023-02-03 16:39:46 -0500

    Cleaned up flow code
    Updated flow file to most recent format, fixed error with 2013 conversion

--- dvnl/flow.py
+++ dvnl/flow.py
@@ -1,8 +1,8 @@
 import os
-import sys
 from pathlib import Path
 from datetime import datetime
 from configparser import ConfigParser
+from typing import List, Literal
 
 from prefect import flow
 from prefect.filesystems import GitHub
@@ -19,7 +19,7 @@ from main import DVNL
 tmp_dir = Path(os.getcwd()) / config["github"]["directory"]
 
 @flow
-def dvnl(raw_dir, output_dir, years, overwrite_download, overwrite_processing, backend, task_runner, run_parallel, max_workers, cores_per_process, log_dir):
+def dvnl(raw_dir: str, output_dir: str, years: List[int], overwrite_download: bool, overwrite_processing: bool, backend: Literal["local", "mpi", "prefect"], task_runner: Literal["sequential", "concurrent", "dask", "hpc"], run_parallel: bool, max_workers: int, cores_per_process: int, log_dir: str):
 
     timestamp = datetime.today()
     time_str = timestamp.strftime("%Y_%m_%d_%H_%M")
@@ -47,4 +47,8 @@ def dvnl(raw_dir, output_dir, years, overwrite_download, overwrite_processing, b
 
     class_instance = DVNL(raw_dir, output_dir, years, overwrite_download, overwrite_processing)
 
-    class_instance.run(backend=backend, task_runner=task_runner, run_parallel=run_parallel, max_workers=max_workers, cores_per_process=cores_per_process, log_dir=timestamp_log_dir, cluster_kwargs=cluster_kwargs)
+    if task_runner != 'hpc':
+        os.chdir(tmp_dir)
+        class_instance.run(backend=backend, task_runner=task_runner, run_parallel=run_parallel, max_workers=max_workers, log_dir=timestamp_log_dir)
+    else:
+        class_instance.run(backend=backend, task_runner=task_runner, run_parallel=run_parallel, max_workers=max_workers, log_dir=timestamp_log_dir, cluster=cluster, cluster_kwargs=cluster_kwargs)
