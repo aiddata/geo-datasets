7f8a06006c005b1f36975fcb492897544722013f
Jacob Hall <email@jacobhall.net>
2023-02-28 15:05:37 -0500

    add memory and cpu requests and limits to kubernetes infra block

--- kubernetes/utilities/create-k8s-job-block.py
+++ kubernetes/utilities/create-k8s-job-block.py
@@ -1,24 +1,67 @@
 from prefect.infrastructure import KubernetesJob
 
-json_patches = [
-    {
-        "op": "add",
-        "path": "/spec/template/spec/volumes",
-        "value": [
-            {"name": "sciclone", "persistentVolumeClaim": {"claimName": "pvc0001"}}
-        ],
-    },
-    {
-        "op": "add",
-        "path": "/spec/template/spec/containers/0/volumeMounts",
-        "value": [{"mountPath": "/sciclone", "name": "sciclone"}],
-    },
-]
+# config
+namespace = "geodata"
+run_image = "jacobwhall/geodata-container"
+
+# should we have requests and limits?
+requests_and_limits: bool = True
+
+# CPU request and limit
+cpu_request: int = 3
+cpu_limit: int = 5
+
+# memory request and limit (in GiB)
+memory_request: int = 1
+memory_limit: int = 3
+
+# patches
+
+json_patches = []
+
+# add CPU and RAM request and limit amounts
+if requests_and_limits:
+    json_patches.extend(
+        [
+            {
+                "op": "add",
+                "path": "/spec/template/spec/containers/0/resources",
+                "value": {
+                    "limits": {
+                        "cpu": str(cpu_limit),
+                        "memory": str(memory_limit) + "Gi",
+                    },
+                    "requests": {
+                        "cpu": str(cpu_request),
+                        "memory": str(memory_request) + "Gi",
+                    },
+                },
+            }
+        ]
+    )
+
+# add volume
+json_patches.extend(
+    [
+        {
+            "op": "add",
+            "path": "/spec/template/spec/volumes",
+            "value": [
+                {"name": "sciclone", "persistentVolumeClaim": {"claimName": "pvc0001"}}
+            ],
+        },
+        {
+            "op": "add",
+            "path": "/spec/template/spec/containers/0/volumeMounts",
+            "value": [{"mountPath": "/sciclone", "name": "sciclone"}],
+        },
+    ]
+)
 
 k8s_job = KubernetesJob(
     name="dataset-run",
-    namespace="geodata",
-    image="localhost/geodata-container",
+    namespace=namespace,
+    image=run_image,
     customizations=json_patches,
 )
-k8s_job.save("kubejob", overwrite=True)
+k8s_job.save("geodata-k8s", overwrite=True)
