b4681456e97ad9249234b8a5f89b6f911a22b238
Jacob Hall <email@jacobhall.net>
2023-03-15 11:43:40 -0400

    use pathlib, simplify module import in global_scripts/deploy.py

--- global_scripts/deploy.py
+++ global_scripts/deploy.py
@@ -1,39 +1,31 @@
 """
-This script deploys a dataset to Prefect, using the settings from its config.ini file.
-
-Basic usage:
-
-`python global_scripts/deploy.py [DATASET]`
-
-You can specify a Kubernetes Job infrastructure block as well.
-Run the following command for more detailed information:
-
-`python global_scripts/deploy.py --help`
+This script is intended to build and deploy a Prefect flow using settings/parameters defined in an
+accompany config.ini for the dataset.
 
 ---------------------------------------
 
-# Roughly equivalent actions via CLI
+Roughly equivalent actions via cli
+#
+# *** the below commands are just a general starting point, and not meant to run as is. note that there
+#     are no parameters specified or storage block creation
 
-*** the below commands are just a general starting point, and not meant to run as is. note that there
-    are no parameters specified or storage block creation
+# to deploy:
+prefect deployment build flow.py:flow_function_name -n "deployment_name" -sb github/existing_storage_block_name -q work_queue_name --apply
 
-## to deploy:
-`prefect deployment build flow.py:flow_function_name -n "deployment_name" -ib infra_block_name -sb github/existing_storage_block_name -q work_queue_name --apply`
+# to not immediately deploy remove `--apply` from the above line, then use the build yaml to run the following:
+# prefect deployment apply build-deployment.yaml
 
-- you can remove "-ib infra_block_name" if you'd like Prefect agent to run the flow locally (rather than on Kubernetes, for example)
-- to not immediately deploy remove `--apply` from the above line, then use the build yaml to run the following:
-  `prefect deployment apply build-deployment.yaml`
+# to run the deployment
+prefect deployment run flow_function_name/deployment_name
 
-## to run the deployment
-`prefect deployment run flow_function_name/deployment_name`
-
-## start agent with correct work queue
-`prefect agent start -q 'work_queue_name'`
+# start workqueue
+prefect agent start -q 'work_queue_name'
 
 """
 
 import os
 import sys
+from pathlib import Path
 from configparser import ConfigParser
 
 import click
@@ -41,31 +33,26 @@ from prefect.filesystems import GitHub
 from prefect.deployments import Deployment
 from prefect.infrastructure.kubernetes import KubernetesJob
 
-def flow_import(module_name, flow_name):
-    module = __import__(module_name)
-    import_flow = getattr(module, flow_name)
-    return import_flow
-
 
 @click.command()
 @click.argument("dataset")
 @click.option("--kubernetes-job-block", default=None, help="Name of Kubernetes Job block to use")
 def deploy(dataset, kubernetes_job_block):
     # find dataset directory
-    dataset_dir = dataset.strip("/")
-    if dataset_dir not in os.listdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))):
-        raise Exception("dataset directory provided not found in current directory")
-    else:
+    dataset_dir = Path(dataset)
+    if dataset_dir.is_dir():
         click.echo(f"Found dataset {dataset}")
+    else:
+        raise Exception("dataset directory provided not found in current directory")
 
     # find and import the get_config_dict function for the dataset
     click.echo("Finding get_config_dict function for dataset...")
-    sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), dataset_dir))
+    sys.path.insert(1, dataset_dir.as_posix())
     from main import get_config_dict
 
     # find and parse dataset config file
     click.echo("Finding config.ini file for dataset...")
-    config_file = dataset_dir + "/config.ini"
+    config_file = dataset_dir / "config.ini"
     config = ConfigParser()
     config.read(config_file)
 
@@ -87,8 +74,9 @@ def deploy(dataset, kubernetes_job_block):
     # block.get_directory(block_repo_dir)
     block.save(block_name, overwrite=True)
 
-    # Driver Code
-    flow = flow_import(module_name, flow_name)
+    # TODO: prevent flow.py from overwriting this file during import
+    module = __import__(module_name)
+    flow = getattr(module, flow_name)
 
     # load a pre-defined block and specify a subfolder of repo
     storage = GitHub.load(block_name)#.get_directory(block_repo_dir)
@@ -101,8 +89,8 @@ def deploy(dataset, kubernetes_job_block):
         for resource in ("cpu", "memory"): 
             config_key = f"{resource}_{request_type}"
             if config.has_option("run", config_key):
-                click.echo(f"Adding resource {request_type}: {amount} for {resouce}")
                 amount = str(config["run"][config_key])
+                click.echo(f"Adding resource {request_type}: {amount} for {resource}")
                 if resource == "memory":
                     amount += "Gi"
                 
