b2ec4b27342c9259d624e2d06bb8e48c9873e3d2
sgoodm <sgoodman@aiddata.wm.edu>
2024-06-25 16:08:04 -0400

    Remove pm25 debug and update gitignore

--- pm25/main.py
+++ pm25/main.py
@@ -105,9 +105,6 @@ class PM25(Dataset):
 
         logger = self.get_logger()
 
-        from boxsdk import JWTAuth, Client
-        logger.debug(JWTAuth.__name__)
-
         # load JWT authentication JSON (see README.md for how to set this up)
         auth = JWTAuth.from_settings_dictionary(self.box_config)
 
