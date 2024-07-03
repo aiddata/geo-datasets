2506633a8d4f6f5468fe5fa893d0d56dc303e366
Jacob Hall <email@jacobhall.net>
2024-07-03 13:00:53 -0400

    code formatting
    - black
    - isort

--- worldpop_age_sex/main.py
+++ worldpop_age_sex/main.py
@@ -5,32 +5,37 @@ worldpop: https://www.worldpop.org/geodata/listing?id=65
 """
 
 import os
-import sys
-import requests
 import shutil
-from pathlib import Path
-from copy import copy
-from pathlib import Path
+import sys
 from configparser import ConfigParser
+from copy import copy
 from datetime import datetime
+from pathlib import Path
 
-sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'global_scripts'))
+import requests
+
+sys.path.insert(
+    1,
+    os.path.join(
+        os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "global_scripts"
+    ),
+)
 
 from data_manager import Dataset
 
 
 class WorldPopAgeSex(Dataset):
-
     name = "WorldPop Age Sex"
 
-    def __init__(self,
-                 process_dir,
-                 raw_dir,
-                 output_dir,
-                 years,
-                 overwrite_download=False,
-                 overwrite_processing=False):
-
+    def __init__(
+        self,
+        process_dir,
+        raw_dir,
+        output_dir,
+        years,
+        overwrite_download=False,
+        overwrite_processing=False,
+    ):
         self.process_dir = Path(process_dir)
         self.raw_dir = Path(raw_dir)
         self.output_dir = Path(output_dir)
@@ -42,35 +47,37 @@ class WorldPopAgeSex(Dataset):
 
         self.template_download_dir_basename = "{SEX}_{AGE}"
 
-
         self.age_list = [0, 1]
         for k in range(5, 85, 5):
             self.age_list.append(k)
 
-        self.sex_list =  ["m", "f"]
-
+        self.sex_list = ["m", "f"]
 
     def test_connection(self):
         # test connection
         test_request = requests.get("https://data.worldpop.org/GIS/", verify=True)
         test_request.raise_for_status()
 
-
     def create_download_list(self):
-
         flist = []
 
         for sex in self.sex_list:
             for age in self.age_list:
-                download_dir = self.template_download_dir_basename.format(SEX = sex, AGE = age)
+                download_dir = self.template_download_dir_basename.format(
+                    SEX=sex, AGE=age
+                )
                 for year in self.years:
-                    src_url = self.template_url.format(SEX = sex, AGE = age, YEAR = year)
-                    tmp_path = self.process_dir / 'download' / download_dir / os.path.basename(src_url)
+                    src_url = self.template_url.format(SEX=sex, AGE=age, YEAR=year)
+                    tmp_path = (
+                        self.process_dir
+                        / "download"
+                        / download_dir
+                        / os.path.basename(src_url)
+                    )
                     dst_path = self.raw_dir / download_dir / os.path.basename(src_url)
                     flist.append((src_url, tmp_path, dst_path))
 
         return flist
-
 
     def manage_download(self, url, tmp_path, dst_path):
         """download individual file using session created
@@ -102,42 +109,46 @@ class WorldPopAgeSex(Dataset):
                     logger.info(f"Copied to dst: {url}")
                     return
 
-
     def download_file(self, url, tmp_path):
         """Download a file from url to tmp_path
         Downloads in chunks
         """
         user_agent_str = "Mozilla/5.0"
-        with requests.get(url, stream=True, verify=True, headers={"User-Agent": user_agent_str}) as r:
+        with requests.get(
+            url, stream=True, verify=True, headers={"User-Agent": user_agent_str}
+        ) as r:
             r.raise_for_status()
-            with open(tmp_path, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=1024*1024):
+            with open(tmp_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=1024 * 1024):
                     f.write(chunk)
-
 
     def move_file(self, src, dst):
         dst.parent.mkdir(parents=True, exist_ok=True)
         shutil.copyfile(src, dst)
-
 
     def create_process_list(self):
         logger = self.get_logger()
 
         flist = []
         # downloaded_files = [i for i in self.raw_dir.iterdir() if str(i).endswith('.tif')]
-        downloaded_files = list(self.raw_dir.glob('**/*.tif'))
+        downloaded_files = list(self.raw_dir.glob("**/*.tif"))
 
         for i in downloaded_files:
-            sex = i.name.split('_')[1]
-            age = int(i.name.split('_')[2])
-            year = int(i.name.split('_')[3])
+            sex = i.name.split("_")[1]
+            age = int(i.name.split("_")[2])
+            year = int(i.name.split("_")[3])
             if sex in self.sex_list and age in self.age_list and year in self.years:
-                flist.append((i, self.process_dir / 'cog_tmp' / i.name, self.output_dir / f'{sex}_{age}' / i.name))
+                flist.append(
+                    (
+                        i,
+                        self.process_dir / "cog_tmp" / i.name,
+                        self.output_dir / f"{sex}_{age}" / i.name,
+                    )
+                )
 
         logger.info(f"COG conversion list: {flist}")
 
         return flist
-
 
     def convert_to_cog(self, src_path, tmp_path, dst_path):
         """
@@ -153,24 +164,24 @@ class WorldPopAgeSex(Dataset):
             logger.info(f"COG Exists: {dst_path}")
 
         else:
-
-            (self.process_dir / 'cog_tmp').mkdir(parents=True, exist_ok=True)
+            (self.process_dir / "cog_tmp").mkdir(parents=True, exist_ok=True)
             (self.output_dir).mkdir(parents=True, exist_ok=True)
 
             logger.info(f"Generating COG: {tmp_path} / {dst_path}")
 
-            with rasterio.open(src_path, 'r') as src:
-
+            with rasterio.open(src_path, "r") as src:
                 profile = copy(src.profile)
 
-                profile.update({
-                    'driver': 'COG',
-                    'compress': 'LZW',
-                    # 'BLOCKXSIZE': 512,
-                    # 'BLOCKYSIZE': 512,
-                    # 'TILED': True,
-                    # 'INTERLEAVE': 'BAND',
-                })
+                profile.update(
+                    {
+                        "driver": "COG",
+                        "compress": "LZW",
+                        # 'BLOCKXSIZE': 512,
+                        # 'BLOCKYSIZE': 512,
+                        # 'TILED': True,
+                        # 'INTERLEAVE': 'BAND',
+                    }
+                )
 
                 # These creation options are not supported by the COG driver
                 # for k in ["BLOCKXSIZE", "BLOCKYSIZE", "TILED", "INTERLEAVE"]:
@@ -180,27 +191,32 @@ class WorldPopAgeSex(Dataset):
                 print(profile)
                 logger.info(profile)
 
-                with rasterio.open(tmp_path, 'w+', **profile) as dst:
-
+                with rasterio.open(tmp_path, "w+", **profile) as dst:
                     for ji, src_window in src.block_windows(1):
                         # convert relative input window location to relative output window location
                         # using real world coordinates (bounds)
-                        src_bounds = windows.bounds(src_window, transform=src.profile["transform"])
-                        dst_window = windows.from_bounds(*src_bounds, transform=dst.profile["transform"])
+                        src_bounds = windows.bounds(
+                            src_window, transform=src.profile["transform"]
+                        )
+                        dst_window = windows.from_bounds(
+                            *src_bounds, transform=dst.profile["transform"]
+                        )
                         # round the values of dest_window as they can be float
-                        dst_window = windows.Window(round(dst_window.col_off), round(dst_window.row_off), round(dst_window.width), round(dst_window.height))
+                        dst_window = windows.Window(
+                            round(dst_window.col_off),
+                            round(dst_window.row_off),
+                            round(dst_window.width),
+                            round(dst_window.height),
+                        )
                         # read data from source window
                         r = src.read(1, window=src_window)
                         # write data to output window
                         dst.write(r, 1, window=dst_window)
 
-
             logger.info(f"Copying COG to final dst: {dst_path}")
             self.move_file(tmp_path, dst_path)
 
-
     def main(self):
-
         logger = self.get_logger()
 
         logger.info("Testing Connection...")
@@ -238,24 +254,36 @@ def get_config_dict(config_file="config.ini"):
         "task_runner": config["run"]["task_runner"],
         "run_parallel": config["run"].getboolean("run_parallel"),
         "max_workers": int(config["run"]["max_workers"]),
-        "log_dir": Path(config["main"]["raw_dir"]) / "logs"
+        "log_dir": Path(config["main"]["raw_dir"]) / "logs",
     }
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     config_dict = get_config_dict()
 
     log_dir = config_dict["log_dir"]
     timestamp = datetime.today()
-    time_format_str: str="%Y_%m_%d_%H_%M"
+    time_format_str: str = "%Y_%m_%d_%H_%M"
     time_str = timestamp.strftime(time_format_str)
     timestamp_log_dir = Path(log_dir) / time_str
     timestamp_log_dir.mkdir(parents=True, exist_ok=True)
 
+    class_instance = WorldPopAgeSex(
+        config_dict["process_dir"],
+        config_dict["raw_dir"],
+        config_dict["output_dir"],
+        config_dict["years"],
+        config_dict["overwrite_download"],
+        config_dict["overwrite_processing"],
+    )
 
-    class_instance = WorldPopAgeSex(config_dict["process_dir"], config_dict["raw_dir"], config_dict["output_dir"], config_dict["years"], config_dict["overwrite_download"], config_dict["overwrite_processing"])
-
-    class_instance.run(backend=config_dict["backend"], task_runner=config_dict["task_runner"], run_parallel=config_dict["run_parallel"], max_workers=config_dict["max_workers"], log_dir=timestamp_log_dir)
+    class_instance.run(
+        backend=config_dict["backend"],
+        task_runner=config_dict["task_runner"],
+        run_parallel=config_dict["run_parallel"],
+        max_workers=config_dict["max_workers"],
+        log_dir=timestamp_log_dir,
+    )
 
 
 else:
@@ -272,10 +300,20 @@ else:
 
         tmp_dir = Path(os.getcwd()) / config["github"]["directory"]
 
-
         @flow
-        def worldpop_age_sex(process_dir, raw_dir, output_dir, years, overwrite_download, overwrite_processing, backend, task_runner, run_parallel, max_workers, log_dir):
-
+        def worldpop_age_sex(
+            process_dir,
+            raw_dir,
+            output_dir,
+            years,
+            overwrite_download,
+            overwrite_processing,
+            backend,
+            task_runner,
+            run_parallel,
+            max_workers,
+            log_dir,
+        ):
             timestamp = datetime.today()
             time_str = timestamp.strftime("%Y_%m_%d_%H_%M")
             timestamp_log_dir = Path(log_dir) / time_str
@@ -302,13 +340,34 @@ else:
                     "conda activate geodata38",
                     f"cd {tmp_dir}",
                 ],
-                "log_directory": str(timestamp_log_dir)
+                "log_directory": str(timestamp_log_dir),
             }
 
-            class_instance = WorldPopAgeSex(process_dir, raw_dir, output_dir, years, overwrite_download, overwrite_processing)
+            class_instance = WorldPopAgeSex(
+                process_dir,
+                raw_dir,
+                output_dir,
+                years,
+                overwrite_download,
+                overwrite_processing,
+            )
 
-            if task_runner != 'hpc':
+            if task_runner != "hpc":
                 os.chdir(tmp_dir)
-                class_instance.run(backend=backend, task_runner=task_runner, run_parallel=run_parallel, max_workers=max_workers, log_dir=timestamp_log_dir)
+                class_instance.run(
+                    backend=backend,
+                    task_runner=task_runner,
+                    run_parallel=run_parallel,
+                    max_workers=max_workers,
+                    log_dir=timestamp_log_dir,
+                )
             else:
-                class_instance.run(backend=backend, task_runner=task_runner, run_parallel=run_parallel, max_workers=max_workers, log_dir=timestamp_log_dir, cluster=cluster, cluster_kwargs=cluster_kwargs)
+                class_instance.run(
+                    backend=backend,
+                    task_runner=task_runner,
+                    run_parallel=run_parallel,
+                    max_workers=max_workers,
+                    log_dir=timestamp_log_dir,
+                    cluster=cluster,
+                    cluster_kwargs=cluster_kwargs,
+                )
