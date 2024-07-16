e6bd5a6b546cc449aeda59dc5c97341abaadf95d
Jacob Hall <email@jacobhall.net>
2024-07-16 12:29:20 -0400

    install data_manager from new path in container prepare script

--- kubernetes/containers/job-runner/prepare.sh
+++ kubernetes/containers/job-runner/prepare.sh
@@ -13,7 +13,7 @@ else
 fi
 
 git clone --branch $branch https://github.com/aiddata/geo-datasets.git --depth 1 || { echo "ERROR: Failed to download data_manager package. Does that tag exist in the git repository? Exiting..."; exit 1; }
-pip install ./geo-datasets/global_scripts/data_manager
+pip install ./geo-datasets/data_manager
 
 # run args
 exec "$@"
